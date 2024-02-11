10f0c5c2e21150ba4e845c6d7a601f040ae7bbba
ahmedsamir42003 <ahmedsamir1042003@gmail.com>
2024-02-11 09:01:18 +0200

    'HackerRank'

--- 1 Week Preparation Kit(HackerRank)/plus-minus/code.py
+++ 1 Week Preparation Kit(HackerRank)/plus-minus/code.py
@@ -14,6 +14,20 @@ import sys
 
 def plusMinus(arr):
     # Write your code here
+    p=0
+    n=0
+    z=0
+    l=len(arr)
+    for i in arr:
+        if i > 0:
+            p=p+1
+        elif i<0:
+            n=n+1
+        else:
+            z=z+1
+    print(round(p/l,6))
+    print(round(n/l,6))
+    print(round(z/l,6))
 
 if __name__ == '__main__':
     n = int(input().strip())
